ecb1265d504e90ca1c74c2fceb2dedf0a28446a0
iNBGi <132969754+iNBGi@users.noreply.github.com>
2023-11-08 20:07:17 +0800

    RUNNING
    NAG RURUN PAG NAKA COMMENT OUT SENSOR YUNG NAGAWA NI DEK

---
+++ react-main/app/screen/Sensor/sensorscreen.js
@@ -0,0 +1,26 @@
+// import React, { useRef, useEffect } from 'react';
+// import { View, StyleSheet } from 'react-native';
+// import { RNCamera } from 'react-native-camera';
+// import * as posenet from '@tensorflow-models/posenet';
+
+// export const Sensorscreen = ({cameraRef, canvasRef }) => {
+  
+//   return (
+//     <View style={styles.container}>
+//       <RNCamera
+//         style={{ flex: 1, alignItems: 'center' }}
+//         ref={ref => {
+//           ref={cameraRef}
+//         }}
+//       />
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+// container: {
+//   flex: 1,
+//   flexDirection: 'column',
+//   backgroundColor: 'black'
+// }
+// })
